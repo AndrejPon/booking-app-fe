587340a5972fa70c66b2a7c11c67e863be1c7407
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-23 18:32:11 +0300

    Header component created

---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  align-items: center;
+  background: lightskyblue;
+  display: flex;
+  font-size: 2rem;
+  font-weight: bold;
+  height: 5rem;
+  justify-content: space-between;
+  line-height: 5rem;
+  padding: 0 1rem;
+  text-align: center;
+`;
