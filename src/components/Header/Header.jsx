64187e5e39a016f3febabd357f3e26af78cf92f5
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-23 18:52:12 +0300

    Navigation component added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as S from './Header.style';
+// import Navigation from '../Navigation/Navigation';
+
+// const links = [
+//   { title: 'Prisijungti', link: '/login' },
+//   { title: 'Registruotis', link: '/register' },
+//   { title: 'My list', link: '/' },
+//   { title: 'Add', link: '/add' },
+// ];
 
 const Header = ({ children }) => {
-  return <S.Header>{children}</S.Header>;
+  return (
+    <S.Header>
+      {children}
+      {/* <Navigation links={links} /> */}
+    </S.Header>
+  );
 };
 
 Header.propTypes = {
