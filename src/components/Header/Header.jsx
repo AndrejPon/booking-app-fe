c18c731545fa6cd4b0dc66557a1aa4508168880f
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-24 10:23:37 +0300

    Header, Navigation components errors fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as S from './Header.style';
-<<<<<<< HEAD
 import Navigation from '../Navigation/Navigation';
 
 const links = [
@@ -10,26 +9,12 @@ const links = [
   { title: 'My list', link: '/' },
   { title: 'Add', link: '/add' },
 ];
-=======
-// import Navigation from '../Navigation/Navigation';
-
-// const links = [
-//   { title: 'Prisijungti', link: '/login' },
-//   { title: 'Registruotis', link: '/register' },
-//   { title: 'My list', link: '/' },
-//   { title: 'Add', link: '/add' },
-// ];
->>>>>>> 64187e5e39a016f3febabd357f3e26af78cf92f5
 
 const Header = ({ children }) => {
   return (
     <S.Header>
       {children}
-<<<<<<< HEAD
       <Navigation links={links} />
-=======
-      {/* <Navigation links={links} /> */}
->>>>>>> 64187e5e39a016f3febabd357f3e26af78cf92f5
     </S.Header>
   );
 };
--- src/components/Navigation/Navigation.style.js
+++ src/components/Navigation/Navigation.style.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-=======
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
->>>>>>> 64187e5e39a016f3febabd357f3e26af78cf92f5
 
 export const Nav = styled.div`
   display: flex;
   column-gap: 0.5rem;
 `;
 
-<<<<<<< HEAD
 export const StyledLink = styled(Link)`
   text-decoration: none;
   font-size: 1rem;
   color: antiquewhite;
-=======
-export const StyledLink = styled.a(Link)`
-text-decoration: none;
-font-size: 1rem;
-color: antiquewhite;
->>>>>>> 64187e5e39a016f3febabd357f3e26af78cf92f5
 `;
